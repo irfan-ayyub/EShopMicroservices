dfca54e272d474d9c8b51221080114d0373af011
irfan-ayyub <mirfan.pucit@gmail.com>
2024-06-30 17:30:08 +0500

    build Catalog.API microservice

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Catalog.API.Products.CreateProduct
+{
+    // data we need to create new product
+    public record CreateProductCommand(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price
+        ) : ICommand<CreateProductResult>;
+
+    // response object, in this case Id
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler(IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // create product entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            // save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            // return result
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
